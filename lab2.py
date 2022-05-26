cbf0879d31ec91ea54a588c1988f3865970f38e1
Suganya <96460553+Suganila@users.noreply.github.com>
2022-05-26 15:15:42 +0530

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,13 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+myValue=12.5
+print(myValue)
+print(type(myValue))
+myValue=22j
+print(myValue)
+print(type(myValue))
+myValue=True
+print(myValue)
+print(type(myValue))
